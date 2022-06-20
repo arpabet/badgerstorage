91ddb029cf47668a006ddb8b637a18a078dc75ff
John Delacion <john@mailnite.com>
2022-06-20 12:46:26 -0700

    small change in comments

--- badger_config.go
+++ badger_config.go
@@ -43,7 +43,7 @@ var (
 	ErrItemNotExist     = errors.New("item not exist")
 )
 
-// Option configures reconciler using the functional options paradigm
+// Option configures badger using the functional options paradigm
 // popularized by Rob Pike and Dave Cheney. If you're unfamiliar with this style,
 // see https://commandcenter.blogspot.com/2014/01/self-referential-functions-and-design.html and
 // https://dave.cheney.net/2014/10/17/functional-options-for-friendly-apis.
